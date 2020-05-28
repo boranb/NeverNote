825ffe58c10cabb3a910233635ee4876fa141034
boranb <berkayboran@gmail.com>
2020-05-28 17:50:45 +0300

    Loin and Logout

--- NeverNote.UI/Scripts/index.js
+++ NeverNote.UI/Scripts/index.js
@@ -5,12 +5,58 @@ var loginData = null;
 
 // Functions
 
-function isLoggedIn() {
-    //todo: sessionstorage ve localstorage da tutulan login bilgilerine bakarak login olup olmadığına karar ver ve eğer loginse uygulamayı aç login değilse login/register sayfasını göster
+function checkLogin() {
+    //sessionstorage ve localstorage da tutulan login bilgilerine bakarak login olup olmadığına karar ver ve eğer loginse uygulamayı aç login değilse login/register sayfasını göster
+
+    var loginData = getLoginData();
+
+    if (!loginData || !loginData.access_token) {
+        showLoginPage();
+        return;
+    }
+    // token'i geçerli mi ?
+    $.ajax({
+        url: apiUrl + "api/Account/UserInfo",
+        type: "GET",
+        headers: { Authorization: "Bearer " + loginData.access_token },
+        success: function (data) {
+            showAppPage();
+        },
+        error: function () {
+            showLoginPage();
+        }
+    });
+
 }
 
-function loginData() {
-    //todo: sessionstorage'da, eğer orada bulamaıysan localstorage'da kayıtlı login data'yı json'dan object'e dönüştür ve yolla eğer yoksa null yolla
+function getLoginData() {
+    //sessionstorage'a bak eğer orada bulamadıysan localstorage'da kayıtlı login data'yı json'dan object'e dönüştür ve yolla eğer yoksa null yolla
+
+    var json = sessionStorage["login"] || localStorage["login"];
+
+    if (json) {
+        try {
+            return JSON.parse(json);
+        } catch (e) {
+            return null;
+        }
+    }
+    return null;
+
+}
+
+function showAppPage() {
+    $(".only-logged-in").show();
+    $(".only-logged-out").hide();
+    $(".page").hide();
+    $("#page-app").show();
+}
+
+function showLoginPage() {
+    $(".only-logged-in").hide();
+    $(".only-logged-out").show();
+    $(".page").hide();
+    $("#page-login").show();
 }
 
 function success(message) {
@@ -59,7 +105,6 @@ function resetLoginForms() {
 }
 
 // Events
-
 $(document).ajaxStart(function () {
     $(".loading").removeClass("d-none");
 });
@@ -68,6 +113,7 @@ $(document).ajaxStop(function () {
     $(".loading").addClass("d-none");
 });
 
+//register
 $("#signupform").submit(function (event) {
     event.preventDefault();
     var formdata = $(this).serialize();
@@ -82,6 +128,7 @@ $("#signupform").submit(function (event) {
 
 });
 
+//login
 $("#signinform").submit(function (event) {
     event.preventDefault();
     var formdata = $(this).serialize();
@@ -98,12 +145,17 @@ $("#signinform").submit(function (event) {
 
         resetLoginForms();
         success("You have been logged in successfully. Redirecting..");
+        setTimeout(function () {
+            resetLoginForms();
+            showAppPage();
+        }, 1000);
 
     }).fail(function (xhr, status, err) {
         errorMessage(xhr.responseJSON.error_description);
     });
 
 });
+
 
 $('#login a[data-toggle="pill"]').on('shown.bs.tab', function (e) {
     // e.target // newly activated tab
@@ -113,7 +165,20 @@ $('#login a[data-toggle="pill"]').on('shown.bs.tab', function (e) {
 });
 
 // https://getbootstrap.com/docs/4.0/components/navs/#via-javascript
-$(".navbar-login a").click(function() {
+$(".navbar-login a").click(function () {
+    event.preventDefault();
     var href = $(this).attr("href");
     $('#pills-tab a[href="' + href + '"]').tab('show'); // Select tab by name
 });
+
+//logOut
+$("#btnLogout").click(function () {
+    event.preventDefault();
+    sessionStorage.removeItem("login");
+    localStorage.removeItem("login");
+    showLoginPage();
+});
+
+
+// Actions
+checkLogin();
